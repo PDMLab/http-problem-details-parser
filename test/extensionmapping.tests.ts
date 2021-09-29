7af2e2bb7082b77622d8c7a52dd69005530f85c9
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-09-29 15:37:20 +0200

    test: sample for usage with union types

--- test/extensionmapping.tests.ts
+++ test/extensionmapping.tests.ts
@@ -2,16 +2,30 @@ import { ProblemDocument, ProblemDocumentExtension } from 'http-problem-details'
 import should from 'should'
 import { fromObject, HttpProblemExtensionMapper, mapExtensions } from '../src'
 import status400 from './json/status-400.json'
+import status404 from './json/status-404.json'
 
-// Idee: Type Guard + Typ vom ProblemDocument type
+type Override<T1, T2> = Omit<T1, keyof T2> & T2
 
-type ValidationProblemDocument = ProblemDocument & {
-  type: 'https://example.net/validation-error'
-  'invalid-params': {
-    name: string
-    reason: string
-  }[]
-}
+type ValidationProblemDocument = Override<
+  ProblemDocument,
+  {
+    type: 'https://example.net/validation-error'
+    'invalid-params': {
+      name: string
+      reason: string
+    }[]
+  }
+>
+
+type NotFoundProblemDocument = Override<
+  ProblemDocument,
+  {
+    status: 404
+    type: 'https://example.net/not-found'
+  }
+>
+
+type Problem = ValidationProblemDocument | NotFoundProblemDocument
 
 function isValidationProblemDocument(
   value: unknown
@@ -54,12 +68,31 @@ describe('type guard', (): void => {
 })
 
 describe('discriminated union', (): void => {
-  describe('should work', (): void => {
-    const document = fromObject(status400, mappers)
-    it('should ', (done) => {
+  describe('should provide extension properties on validation error', (): void => {
+    it('should provide extension properties', (done) => {
+      const document = fromObject(status400, mappers) as Problem
       switch (document.type) {
         case 'https://example.net/validation-error':
           document['invalid-params'].length.should.equal(2)
+          break
+        case 'https://example.net/not-found':
+          should.not.exist(document['invalid-params'])
+          break
+
+        default:
+          break
+      }
+
+      done()
+    })
+    it('should not provide extension properties on 404 error', (done) => {
+      const document = fromObject(status404, mappers) as Problem
+      switch (document.type) {
+        case 'https://example.net/validation-error':
+          document['invalid-params'].length.should.equal(2)
+          break
+        case 'https://example.net/not-found':
+          should.not.exist(document['invalid-params'])
           break
 
         default:
